e8a374ab9edfc570791c68484c3f2d67bd5fad46
Evin <evinodisho@gmail.com>
2025-02-02 18:51:26 -0800

    Added function prototypes for constructor and destructor

--- hash_table.h
+++ hash_table.h
@@ -5,3 +5,20 @@
 
 typedef struct table table_t;
 
+/**
+ * @brief Hash table constructor
+ * 
+ * @param size_t size of table (array length)
+ * 
+ * @return *table_t Pointer to new table
+ */
+table_t *NewTable(size_t size);
+
+/**
+ * @brief Hash table destructor
+ * 
+ * @param **table_t Hash table to delete
+ * 
+ * @return void
+ */
+void DeleteTable(table_t **table);
